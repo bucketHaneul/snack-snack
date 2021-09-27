acb66a36969251702e8bbcd6047df5513db2734e
HyuuunjuKim <gus7wn@gmail.com>
2021-09-28 02:30:34 +0900

    fix: Move Snackbar, SnackbarPortal to Provider

--- src/SnackbarProvider.js
+++ src/SnackbarProvider.js
@@ -1,6 +1,8 @@
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import SnackbarContext from './SnackbarContext';
+import { default as SnackBarComponent } from './Snackbar/Snackbar';
 
 const SnackbarProvider = ({ children }) => {
   const [snackbarMessage, setSnackbarMessage] = useState('원하는 메시지를 입력하세요!');
@@ -8,7 +10,21 @@ const SnackbarProvider = ({ children }) => {
 
   const store = { snackbarMessage, setSnackbarMessage, isSnackbarOpen, setSnackbarOpen };
 
-  return <SnackbarContext.Provider value={store}>{children}</SnackbarContext.Provider>;
+  const SnackbarPortal = ({ children }) =>
+    ReactDOM.createPortal(children, document.getElementById('snackbar'));
+
+  const SnackBar = () => (
+    <SnackbarPortal>
+      <SnackBarComponent>{snackbarMessage}</SnackBarComponent>
+    </SnackbarPortal>
+  );
+
+  return (
+    <SnackbarContext.Provider value={store}>
+      {children}
+      {isSnackbarOpen && <SnackBar />}
+    </SnackbarContext.Provider>
+  );
 };
 
 SnackbarProvider.propTypes = {
--- src/useSnackbar.js
+++ src/useSnackbar.js
@@ -1,20 +1,8 @@
-import React from 'react';
 import { useContext, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { default as SnackBarComponent } from './Snackbar/Snackbar';
 import SnackbarContext from './SnackbarContext';
 
 const useSnackBar = () => {
-  const { snackbarMessage, setSnackbarMessage, isSnackbarOpen, setSnackbarOpen } =
-    useContext(SnackbarContext);
-  const SnackbarPortal = ({ children }) =>
-    ReactDOM.createPortal(children, document.getElementById('snackbar'));
-
-  const SnackBar = () => (
-    <SnackbarPortal>
-      <SnackBarComponent>{snackbarMessage}</SnackBarComponent>
-    </SnackbarPortal>
-  );
+  const { setSnackbarMessage, isSnackbarOpen, setSnackbarOpen } = useContext(SnackbarContext);
 
   const openSnackBar = (newMessage) => {
     setSnackbarMessage(newMessage);
@@ -25,14 +13,14 @@ const useSnackBar = () => {
     if (!isSnackbarOpen) return;
 
     const intervalId = setInterval(() => {
-      setSnackbarMessage('newMessage');
+      setSnackbarMessage('');
       setSnackbarOpen(false);
     }, 3000);
 
     return () => clearInterval(intervalId);
   }, [isSnackbarOpen]);
 
-  return { isSnackbarOpen, openSnackBar, SnackBar };
+  return { isSnackbarOpen, openSnackBar };
 };
 
 export default useSnackBar;
